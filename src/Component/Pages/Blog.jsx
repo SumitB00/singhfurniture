df51fdcfe362d51f78385ade22ec28e84a2cd681
SumitB00 <ayushbhadri03@gmail.com>
2025-03-25 09:42:15 +0530

    some changest

--- src/Component/Pages/Blog.jsx
+++ src/Component/Pages/Blog.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import banner from '../assets/blog banner.png'
-import lapi from '../assets/lapi.png'
-import write from '../assets/write.png'
-import tab from '../assets/tab.png'
-import sum1 from '../assets/blog1.png'
-import sum2 from '../assets/blog2.png'
-import sum3 from '../assets/blog3.png'
-import sum4 from '../assets/blog4.png'
-import sum5 from '../assets/blog5.png'
+import Banner from '../assets/blog banner.png'
+import Lapi from '../assets/lapi.png'
+import Write from '../assets/write.png'
+import Tab from '../assets/tab.png'
+import Sum1 from '../assets/blog1.png'
+import Sum2 from '../assets/blog2.png'
+import Sum3 from '../assets/blog3.png'
+import Sum4 from '../assets/blog4.png'
+import Sum5 from '../assets/blog5.png'
 export const Blog = () => {
   return (
     <>
-   <img src={banner} class="img-fluid w-100" alt="Banner" />
+   <img src={Banner} class="img-fluid w-100" alt="Banner" />
    <section class="container my-5">
         <div class="row">
             <div class="col-lg-8 col-md-7 col-sm-12">
                 <div class="mb-5">
-                    <img src={lapi} class="img-fluid" alt="Blog Image"/>
+                    <img src={Lapi} class="img-fluid" alt="Blog Image"/>
                     <p class="text-muted mt-2">
                         <i class="bi bi-person-fill"></i> Admin 
                         <i class="bi bi-calendar-fill ms-3"></i> 14 OCT 2022 
@@ -27,7 +27,7 @@ export const Blog = () => {
                     <a href="#" class="text-decoration-underline text-dark h5">Read More</a>
                 </div>
                 <div class="mb-5">
-                    <img src={write} class="img-fluid" alt="Blog Image"/>
+                    <img src={Write} class="img-fluid" alt="Blog Image"/>
                     <p class="text-muted mt-2">
                         <i class="bi bi-person-fill"></i> Admin 
                         <i class="bi bi-calendar-fill ms-3"></i> 14 OCT 2022 
@@ -38,7 +38,7 @@ export const Blog = () => {
                     <a href="#" class="text-decoration-underline text-dark h5">Read More</a>
                 </div>
                 <div>
-                    <img src={tab} class="img-fluid" alt="Blog Image"/>
+                    <img src={Tab} class="img-fluid" alt="Blog Image"/>
                     <p class="text-muted mt-2">
                         <i class="bi bi-person-fill"></i> Admin 
                         <i class="bi bi-calendar-fill ms-3"></i> 14 OCT 2022 
@@ -60,35 +60,35 @@ export const Blog = () => {
                 </ul>
                 <h4 class="mt-4">Recent Posts</h4>
                 <div class="d-flex align-items-center my-3">
-                    <img src={sum1} class="img-thumbnail" width="80"/>
+                    <img src={Sum1} class="img-thumbnail" width="80"/>
                     <div class="ms-3">
                         <h6>Exploring new ways of decorating</h6>
                         <p class="text-muted">03 Aug 2022</p>
                     </div>
                 </div>
                 <div class="d-flex align-items-center my-3">
-                    <img src={sum2} class="img-thumbnail" width="80"/>
+                    <img src={Sum2} class="img-thumbnail" width="80"/>
                     <div class="ms-3">
                         <h6>Handmade pieces that took time to make</h6>
                         <p class="text-muted">03 Aug 2022</p>
                     </div>
                 </div>
                 <div class="d-flex align-items-center my-3">
-                    <img src={sum3} class="img-thumbnail" width="80"/>
+                    <img src={Sum3} class="img-thumbnail" width="80"/>
                     <div class="ms-3">
                         <h6>Modern home in Milan</h6>
                         <p class="text-muted">03 Aug 2022</p>
                     </div>
                 </div>
                 <div class="d-flex align-items-center my-3">
-                    <img src={sum4} class="img-thumbnail" width="80"/>
+                    <img src={Sum4} class="img-thumbnail" width="80"/>
                     <div class="ms-3">
                         <h6>Going all-in with millennial design</h6>
                         <p class="text-muted">03 Aug 2022</p>
                     </div>
                 </div>
                 <div class="d-flex align-items-center my-3">
-                    <img src={sum5} class="img-thumbnail" width="80"/>
+                    <img src={Sum5} class="img-thumbnail" width="80"/>
                     <div class="ms-3">
                         <h6>Colorful office redesign</h6>
                         <p class="text-muted">03 Aug 2022</p>
